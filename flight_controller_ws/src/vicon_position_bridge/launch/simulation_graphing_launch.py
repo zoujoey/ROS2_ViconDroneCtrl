0e641eb13b5db9b4a54e3d9bdc842092883b791a
zoujoey <joezou5555@gmail.com>
2023-09-05 16:23:27 -0400

    split graphing_launch.py

---
+++ flight_controller_ws/src/vicon_position_bridge/launch/simulation_graphing_launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+    ld = LaunchDescription()
+    
+    hostname = '10.42.0.130'
+    buffer_size = 1024
+    topic_namespace = 'vicon'
+    simulation = True
+    if simulation:
+        fake_vicon_position_node = Node(
+                package="vicon_position_bridge",
+                executable = "fake_pose_pub",
+            )
+        ld.add_action(fake_vicon_position_node)
+    else:
+        vicon_wrapper_node = Node(
+                package='vicon_receiver', executable='vicon_client', output='screen',
+                parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
+            )
+        ld.add_action(vicon_wrapper_node)
+
+        pose_pub = Node(
+                package = "vicon_position_bridge",
+                executable = "pose_pub"
+        )
+
+        ld.add_action(pose_pub)
+    
+    # pose_data_save = Node(
+    #     package = "vicon_position_bridge",
+    #     executable = "pose_data_save"
+    # )
+    # ld.add_action(pose_data_save)
+
+    # pose_grapher_xyz = Node(
+    #     package = "vicon_position_bridge",
+    #     executable = "pose_grapher_xyz"
+    # )
+    # ld.add_action(pose_grapher_xyz)
+
+    # pose_grapher_xy = Node(
+    #     package = "vicon_position_bridge",
+    #     executable = "pose_grapher_xy"
+    # )
+    # ld.add_action(pose_grapher_xy)
+
+    # pose_grapher_yz = Node(
+    #     package = "vicon_position_bridge",
+    #     executable = "pose_grapher_yz"
+    # )
+    # ld.add_action(pose_grapher_yz)
+
+    # pose_grapher_xz = Node(
+    #     package = "vicon_position_bridge",
+    #     executable = "pose_grapher_xz"
+    # )
+    # ld.add_action(pose_grapher_xz)
+
+    return ld
